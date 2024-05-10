8cd0b42e7f0736760f85a225b22cd122d564ba0c
Tom Pillet <tom.pillet-gaulon@capgemini.com>
2024-05-11 01:33:06 +0200

    work on main infos : new footer, rules btn, reword html structure and header css

--- rock-paper-scissors-app/src/App.tsx
+++ rock-paper-scissors-app/src/App.tsx
@@ -1,38 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   
   return (
     <>
-    <header className='game-header'>
-      <div className="game-title">ROCK<br/>PAPER<br/>SCISSORS</div>
-      <div className="game-score">SCORE <span id="score">12</span></div>
-    </header>
     
-    <div className='container'>
+    <div className='game-container'>
+      <header className='game-header'>
+        <div className="game-title">ROCK PAPER SCISSORS</div>
+        <div className="game-score">SCORE <span id="score">12</span></div>
+      </header>
       
-      <div className="round rock">
-        <p>rock</p>
+      <div className="game-board">
+        <div className="round rock">
+          <p>rock</p>
+        </div>
+        <div className="round paper">
+          <p>paper</p>
+        </div>
+        {/* <div className="round scissors">
+          <p>scissors</p>
+        </div>
+        <div className="round lizard">
+          <p>lizard</p>
+        </div>
+        <div className="round spock">
+          <p>spock</p>
+        </div>*/}
       </div>
-      <div className="round paper">
-        <p>paper</p>
-      </div>
-      {/* <div className="round scissors">
-        <p>scissors</p>
-      </div>
-      <div className="round lizard">
-        <p>lizard</p>
-      </div>
-      <div className="round spock">
-        <p>spock</p>
-      </div>
-     */}
     </div>
-    
+
+     <footer className='game-footer'>
+      <div className="game-rules">
+        <button id="btn-rules">RULES</button>
+      </div>
+     </footer>
     </>
     )
   }
